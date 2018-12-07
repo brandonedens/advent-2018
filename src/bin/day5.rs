22aa9f1c019ba20479f95a6058ad9f4a3e361f18
Brandon Edens <brandonedens@gmail.com>
2018-12-06 23:11:41 -0800

    Add solution to day 5 problem 2.

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -11,10 +11,9 @@
 
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
 
 fn problem1() {
-    let mut file = File::open("input/day5.txt").unwrap();
+    let file = File::open("input/day5.txt").unwrap();
 
     let mut v: Vec<char> = Vec::new();
     for c in file.bytes() {
@@ -44,6 +43,50 @@ fn problem1() {
     println!("len: {}", v.len());
 }
 
+fn problem2() {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let mut counts: Vec<(usize, char)> = Vec::new();
+
+    for a in alphabet.chars() {
+        let mut v: Vec<char> = Vec::new();
+
+        let file = File::open("input/day5.txt").unwrap();
+        for c in file.bytes() {
+            let c = c.unwrap() as char;
+
+            if !c.is_ascii_alphabetic() {
+                continue;
+            }
+
+            if c.eq_ignore_ascii_case(&a) {
+                continue;
+            }
+
+            if v.len() == 0 {
+                v.push(c);
+                continue;
+            }
+
+            let last = v.last().unwrap();
+            if c.eq_ignore_ascii_case(last)
+                && ((c.is_ascii_uppercase() && last.is_ascii_lowercase())
+                    || (last.is_ascii_uppercase() && c.is_ascii_lowercase()))
+            {
+                // We have aA or Aa.
+                v.pop();
+                continue;
+            }
+
+            v.push(c);
+        }
+        counts.push((v.len(), a));
+    }
+    counts.sort();
+
+    println!("{:?}", counts.first().unwrap());
+}
+
 fn main() {
     problem1();
+    problem2();
 }
